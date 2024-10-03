cf400fdced8d1fabeca573755fb95bc85e7413b4
Oleg <evolrn89@gmail.com>
2024-10-03 16:47:01 +0600

    Created component UserItem to display user info

---
+++ src/components/User/UserItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IUser } from  "../../types";
+
+interface Props {
+  user: IUser;
+}
+
+const UserItem: React.FC<Props> = ({ user }) => {
+  return (
+    <li className="list-group-item">
+      <div><strong>Name:</strong> {user.name}</div>
+      <div><strong>Email:</strong> {user.email}</div>
+      <div><strong>Active:</strong> {user.active ? 'Yes' : 'No'}</div>
+      <div><strong>Role:</strong> {user.role}</div>
+    </li>
+  );
+};
+
+export default UserItem;
